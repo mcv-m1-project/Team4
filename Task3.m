87f620bf201fabb5e0d405f081cf23ab422b1c15
tpereztorres <tonipereztorres@gmail.com>
2016-10-12 17:02:01 +0200

    Add files via upload

--- Task3.m
+++ Task3.m
@@ -1,9 +1,3 @@
-im = imread('/Users/Toni/Desktop/Toni/Master/ProjectMaterials/Week 1/Code/train/00.001147.jpg'); % read the image
-
-dirname = '/Users/Toni/Desktop/Toni/Master/ProjectMaterials/Week 1/Code/Task1/train_split';
-dirname_new = ['/Users/Toni/Desktop/Toni/Master/ProjectMaterials/Week 1/Code/Task1/train_split/mask' ];
-[s, mess, messid] = mkdir(dirname_new);
-
 createMask(A, train_split_A, [dirname '/A'], 'A');
 createMask(B, train_split_B, [dirname '/B'], 'B');
 createMask(C, train_split_C, [dirname '/C'], 'C');
