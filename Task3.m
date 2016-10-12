b6eae9a12f03c3727c32505c2d9eb0f2f3952af3
tpereztorres <tonipereztorres@gmail.com>
2016-10-12 18:06:50 +0200

    Add files via upload

--- Task3.m
+++ Task3.m
@@ -1,7 +1,9 @@
-createMask(A, train_split_A, [dirname '/A'], 'A');
-createMask(B, train_split_B, [dirname '/B'], 'B');
-createMask(C, train_split_C, [dirname '/C'], 'C');
-createMask(D, train_split_D, [dirname '/D'], 'D');
-createMask(E, train_split_E, [dirname '/E'], 'E');
-createMask(F, train_split_F, [dirname '/F'], 'F');
+dirname = '/Users/Toni/Desktop/Toni/Master/ProjectMaterials/Week 1/Code/Task1/train_split';
+
+createMask(sA, train_split_A, [dirname '/A'], 'A');
+createMask(sB, train_split_B, [dirname '/B'], 'B');
+createMask(sC, train_split_C, [dirname '/C'], 'C');
+createMask(sD, train_split_D, [dirname '/D'], 'D');
+createMask(sE, train_split_E, [dirname '/E'], 'E');
+createMask(sF, train_split_F, [dirname '/F'], 'F');
 
