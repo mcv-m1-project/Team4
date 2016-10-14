9fd8c63d1856a1abce84e56941517afb279301eb
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-14 18:41:13 +0200

    Update TesteigImatges.m

--- TesteigImatges.m
+++ TesteigImatges.m
@@ -1,6 +1,8 @@
 function [Metodo] = TesteigImatges (struct, train_split,dirname,dirname_new,pixel_method)
 
-
+kRGB=0;
+kHSV=0;
+kLab=0;
 switch pixel_method
     case 'RGB'
         
@@ -8,14 +10,15 @@ switch pixel_method
     
         toSplit = strsplit(struct{i}.name,{'gt.','.txt'}); 
 
-        mask_truth = imread(fullfile([dirname '/mask'], strjoin(['mask.' toSplit(2) '.png'],'')));
+        mask_truth = imread(fullfile([dirname], strjoin(['mask.' toSplit(2) '.png'],'')));
         mask_totest= imread(fullfile(dirname_new, strjoin(['mask_rgb_' toSplit(2) '.png'],'')));
         
         [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(mask_totest, mask_truth);
         [pixelPrecision, pixelAccuracy, pixelSpecificity, pixelSensitivity] = PerformanceEvaluationPixel(pixelTP, pixelFP, pixelFN, pixelTN);
         
-        
-        Metodo{i,1}=struct('Number TP', pixelTP, 'Number FP', pixelFP, 'Number FN', pixelFN, 'Number TN', pixelTN, 'Precision', pixelPrecision, 'Accuracy', pixelAccuracy, 'Specifity', pixelSpecificity, 'Sensivity', pixelSensitivity);
+        kRGB=kRGB+1;
+        Metodo{kRGB,1}=struct('NumberTP', pixelTP, 'NumberFP', pixelFP, 'NumberFN', pixelFN, 'NumberTN', pixelTN, 'Precision', pixelPrecision, 'Accuracy', pixelAccuracy, 'Specifity', pixelSpecificity, 'Sensivity', pixelSensitivity);
+       
         end
         
      case 'HSV'
@@ -30,8 +33,9 @@ switch pixel_method
         [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(mask_totest, mask_truth);
         [pixelPrecision, pixelAccuracy, pixelSpecificity, pixelSensitivity] = PerformanceEvaluationPixel(pixelTP, pixelFP, pixelFN, pixelTN);
         
-        
-        Metodo{i,1}=struct('Number TP', pixelTP, 'Number FP', pixelFP, 'Number FN', pixelFN, 'Number TN', pixelTN, 'Precision', pixelPrecision, 'Accuracy', pixelAccuracy, 'Specifity', pixelSpecificity, 'Sensivity', pixelSensitivity);
+        kHSV=kHSV+1;        
+        Metodo{kHSV,1}=struct('Number TP', pixelTP, 'Number FP', pixelFP, 'Number FN', pixelFN, 'Number TN', pixelTN, 'Precision', pixelPrecision, 'Accuracy', pixelAccuracy, 'Specifity', pixelSpecificity, 'Sensivity', pixelSensitivity);
+       
         end
         
      case 'Lab'
@@ -46,10 +50,12 @@ switch pixel_method
         [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(mask_totest, mask_truth);
         [pixelPrecision, pixelAccuracy, pixelSpecificity, pixelSensitivity] = PerformanceEvaluationPixel(pixelTP, pixelFP, pixelFN, pixelTN);
         
-        
-        Metodo{i,1}=struct('Number TP', pixelTP, 'Number FP', pixelFP, 'Number FN', pixelFN, 'Number TN', pixelTN, 'Precision', pixelPrecision, 'Accuracy', pixelAccuracy, 'Specifity', pixelSpecificity, 'Sensivity', pixelSensitivity);
+        kLab=kLab+1
+        Metodo{kLab,1}=struct('Number TP', pixelTP, 'Number FP', pixelFP, 'Number FN', pixelFN, 'Number TN', pixelTN, 'Precision', pixelPrecision, 'Accuracy', pixelAccuracy, 'Specifity', pixelSpecificity, 'Sensivity', pixelSensitivity);
             
         end
 end
+
+
 
         
