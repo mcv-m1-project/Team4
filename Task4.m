c7fdf22948938324bdc0729d754ed84d125c39a0
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-14 10:58:11 +0200

    Add files via upload

---
+++ Task4.m
@@ -0,0 +1,27 @@
+dirname = 'train';
+dirname_new = 'train_split/mask';
+
+Metodo_RGB_A = TesteigImatges(sA,tA,[dirname '/A'],[dirname_new 'A'],'RGB');
+Metodo_HSV_A = TesteigImatges(sA,tA,[dirname '/A'],[dirname_new 'A'],'HSV');
+Metodo_Lab_A = TesteigImatges(sA,tA,[dirname '/A'],[dirname_new 'A'],'Lab');
+
+Metodo_RGB_B = TesteigImatges(sB,tB,[dirname '/B'],[dirname_new 'B'],'RGB');
+Metodo_HSV_B = TesteigImatges(sB,tB,[dirname '/B'],[dirname_new 'B'],'HSV');
+Metodo_Lab_B = TesteigImatges(sB,tB,[dirname '/B'],[dirname_new 'B'],'Lab');
+
+Metodo_RGB_C = TesteigImatges(sC,tC,[dirname '/C'],[dirname_new 'C'],'RGB');
+Metodo_HSV_C = TesteigImatges(sC,tC,[dirname '/C'],[dirname_new 'C'],'HSV');
+Metodo_Lab_C = TesteigImatges(sC,tC,[dirname '/C'],[dirname_new 'C'],'Lab');
+
+Metodo_RGB_D = TesteigImatges(sD,tD,[dirname '/D'],[dirname_new 'D'],'RGB');
+Metodo_HSV_D = TesteigImatges(sD,tD,[dirname '/D'],[dirname_new 'D'],'HSV');
+Metodo_Lab_D = TesteigImatges(sD,tD,[dirname '/D'],[dirname_new 'D'],'Lab');
+
+Metodo_RGB_E = TesteigImatges(sE,tE,[dirname '/E'],[dirname_new 'E'],'RGB');
+Metodo_HSV_E = TesteigImatges(sE,tE,[dirname '/E'],[dirname_new 'E'],'HSV');
+Metodo_Lab_E = TesteigImatges(sE,tE,[dirname '/E'],[dirname_new 'E'],'Lab');
+
+Metodo_RGB_F = TesteigImatges(sF,tF,[dirname '/F'],[dirname_new 'F'],'RGB');
+Metodo_HSV_F = TesteigImatges(sF,tF,[dirname '/F'],[dirname_new 'F'],'HSV');
+Metodo_Lab_F = TesteigImatges(sF,tF,[dirname '/F'],[dirname_new 'F'],'Lab');
+
