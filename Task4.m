bfba4c49a149cb2f252ee797ea69ea0c50985a62
tpereztorres <tonipereztorres@gmail.com>
2016-10-15 20:41:22 +0200

    Add files via upload

--- Task4.m
+++ Task4.m
@@ -1,27 +1,52 @@
-dirname = 'train';
+dirname = '/Users/Toni/Desktop/Toni/Master/ProjectMaterials/Week 1/Code/train';
 dirname_new = 'train_split/mask';
 
-Metodo_RGB_A = TesteigImatges(sA,tA,[dirname '/mask'],[dirname_new '/A'],'RGB');
-Metodo_HSV_A = TesteigImatges(sA,tA,[dirname '/mask'],[dirname_new '/A'],'HSV');
-Metodo_Lab_A = TesteigImatges(sA,tA,[dirname '/mask'],[dirname_new '/A'],'Lab');
+Metodo_RGB_A = TesteigImatges(sA,train_split_A,[dirname '/mask'],[dirname_new '/A'],'RGB');
+Metodo_HSV_A = TesteigImatges(sA,train_split_A,[dirname '/mask'],[dirname_new '/A'],'HSV');
+Metodo_Lab_A = TesteigImatges(sA,train_split_A,[dirname '/mask'],[dirname_new '/A'],'Lab');
 
-Metodo_RGB_B = TesteigImatges(sB,tB,[dirname '/mask'],[dirname_new '/B'],'RGB');
-Metodo_HSV_B = TesteigImatges(sB,tB,[dirname '/mask'],[dirname_new '/B'],'HSV');
-Metodo_Lab_B = TesteigImatges(sB,tB,[dirname '/mask'],[dirname_new '/B'],'Lab');
+Metodo_RGB_B = TesteigImatges(sB,train_split_B,[dirname '/mask'],[dirname_new '/B'],'RGB');
+Metodo_HSV_B = TesteigImatges(sB,train_split_B,[dirname '/mask'],[dirname_new '/B'],'HSV');
+Metodo_Lab_B = TesteigImatges(sB,train_split_B,[dirname '/mask'],[dirname_new '/B'],'Lab');
 
-Metodo_RGB_C = TesteigImatges(sC,tC,[dirname '/mask'],[dirname_new '/C'],'RGB');
-Metodo_HSV_C = TesteigImatges(sC,tC,[dirname '/mask'],[dirname_new '/C'],'HSV');
-Metodo_Lab_C = TesteigImatges(sC,tC,[dirname '/mask'],[dirname_new '/C'],'Lab');
+Metodo_RGB_C = TesteigImatges(sC,train_split_C,[dirname '/mask'],[dirname_new '/C'],'RGB');
+Metodo_HSV_C = TesteigImatges(sC,train_split_C,[dirname '/mask'],[dirname_new '/C'],'HSV');
+Metodo_Lab_C = TesteigImatges(sC,train_split_C,[dirname '/mask'],[dirname_new '/C'],'Lab');
 
-Metodo_RGB_D = TesteigImatges(sD,tD,[dirname '/mask'],[dirname_new '/D'],'RGB');
-Metodo_HSV_D = TesteigImatges(sD,tD,[dirname '/mask'],[dirname_new '/D'],'HSV');
-Metodo_Lab_D = TesteigImatges(sD,tD,[dirname '/mask'],[dirname_new '/D'],'Lab');
+Metodo_RGB_D = TesteigImatges(sD,train_split_D,[dirname '/mask'],[dirname_new '/D'],'RGB');
+Metodo_HSV_D = TesteigImatges(sD,train_split_D,[dirname '/mask'],[dirname_new '/D'],'HSV');
+Metodo_Lab_D = TesteigImatges(sD,train_split_D,[dirname '/mask'],[dirname_new '/D'],'Lab');
 
-Metodo_RGB_E = TesteigImatges(sE,tE,[dirname '/mask'],[dirname_new '/E'],'RGB');
-Metodo_HSV_E = TesteigImatges(sE,tE,[dirname '/mask'],[dirname_new '/E'],'HSV');
-Metodo_Lab_E = TesteigImatges(sE,tE,[dirname '/mask'],[dirname_new '/E'],'Lab');
+Metodo_RGB_E = TesteigImatges(sE,train_split_E,[dirname '/mask'],[dirname_new '/E'],'RGB');
+Metodo_HSV_E = TesteigImatges(sE,train_split_E,[dirname '/mask'],[dirname_new '/E'],'HSV');
+Metodo_Lab_E = TesteigImatges(sE,train_split_E,[dirname '/mask'],[dirname_new '/E'],'Lab');
 
-Metodo_RGB_F = TesteigImatges(sF,tF,[dirname '/mask'],[dirname_new '/F'],'RGB');
-Metodo_HSV_F = TesteigImatges(sF,tF,[dirname '/mask'],[dirname_new '/F'],'HSV');
-Metodo_Lab_F = TesteigImatges(sF,tF,[dirname '/mask'],[dirname_new '/F'],'Lab');
+Metodo_RGB_F = TesteigImatges(sF,train_split_F,[dirname '/mask'],[dirname_new '/F'],'RGB');
+Metodo_HSV_F = TesteigImatges(sF,train_split_F,[dirname '/mask'],[dirname_new '/F'],'HSV');
+Metodo_Lab_F = TesteigImatges(sF,train_split_F,[dirname '/mask'],[dirname_new '/F'],'Lab');
 
+% RGB
+RGB_A_avg = calculateAverage(Metodo_RGB_A);
+RGB_B_avg = calculateAverage(Metodo_RGB_B);
+RGB_C_avg = calculateAverage(Metodo_RGB_C);
+RGB_D_avg = calculateAverage(Metodo_RGB_D);
+RGB_E_avg = calculateAverage(Metodo_RGB_E);
+RGB_F_avg = calculateAverage(Metodo_RGB_F);
+
+
+
+% HSV
+HSV_A_avg = calculateAverage(Metodo_HSV_A);
+HSV_B_avg = calculateAverage(Metodo_HSV_B);
+HSV_C_avg = calculateAverage(Metodo_HSV_C);
+HSV_D_avg = calculateAverage(Metodo_HSV_D);
+HSV_E_avg = calculateAverage(Metodo_HSV_E);
+HSV_F_avg = calculateAverage(Metodo_HSV_F);
+
+% Lab
+Lab_A_avg = calculateAverage(Metodo_Lab_A);
+Lab_B_avg = calculateAverage(Metodo_Lab_B);
+Lab_C_avg = calculateAverage(Metodo_Lab_C);
+Lab_D_avg = calculateAverage(Metodo_Lab_D);
+Lab_E_avg = calculateAverage(Metodo_Lab_E);
+Lab_F_avg = calculateAverage(Metodo_Lab_F);
--- calculateAverage.m
+++ calculateAverage.m
@@ -1,18 +1,18 @@
 function [Metodo]= calculateAverage(method)
 
 for i = 1:length(method)
-    RGB_TP = sum(method{i,1}.NumberTP);
-    RGB_FP = sum(method{i,1}.NumberFP);
-    RGB_FN = sum(method{i,1}.NumberFN);
-    RGB_TN = sum(method{i,1}.NumberTN);
-    RGB_Precision = mean(method{i,1}.Precision);
-    RGB_Accuracy = mean(method{i,1}.Accuracy);
-    RGB_Specificity = mean(method{i,1}.Specifity);
-    RGB_Sensitivity = mean(method{i,1}.Sensivity);
-    RGB_F1 = mean(method{i,1}.F1);
-    RGB_Recall = mean(method{i,1}.Recall);
+    TP = sum(method{i,1}.NumberTP);
+    FP = sum(method{i,1}.NumberFP);
+    FN = sum(method{i,1}.NumberFN);
+    TN = sum(method{i,1}.NumberTN);
+    Precision = mean(method{i,1}.Precision);
+    Accuracy = mean(method{i,1}.Accuracy);
+    Specificity = mean(method{i,1}.Specifity);
+    Sensitivity = mean(method{i,1}.Sensivity);
+    F1 = mean(method{i,1}.F1);
+    Recall = mean(method{i,1}.Recall);
     
-    Metodo = struct('NumberTP', RGB_TP, 'NumberFP', RGB_FP, 'NumberFN', RGB_FN, 'NumberTN', RGB_TN, 'Precision', RGB_Precision, 'Accuracy', RGB_Accuracy, 'Specifity', RGB_Specificity, 'Sensivity', RGB_Sensitivity,'F1', RGB_F1,'Recall', RGB_Recall);
+    Metodo = struct('NumberTP', TP, 'NumberFP', FP, 'NumberFN', FN, 'NumberTN', TN, 'Precision', Precision, 'Accuracy', Accuracy, 'Specifity', Specificity, 'Sensivity', Sensitivity,'F1', F1,'Recall', Recall);
 
 end
 end
