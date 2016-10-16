a72e73f2cb63ec03fa34dfc48f42302a59867d12
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-16 16:22:49 +0200

    Update calculateAverage.m

--- calculateAverage.m
+++ calculateAverage.m
@@ -1,18 +1,38 @@
 function [Metodo]= calculateAverage(method)
+TP=0;
+FP=0;
+FN=0;
+TN=0;
+Precision=0;
+Accuracy=0;
+Specifity=0;
+F1=0;
+Recall=0;
 
 for i = 1:length(method)
-    TP = sum(method{i,1}.NumberTP);
-    FP = sum(method{i,1}.NumberFP);
-    FN = sum(method{i,1}.NumberFN);
-    TN = sum(method{i,1}.NumberTN);
-    Precision = mean(method{i,1}.Precision);
-    Accuracy = mean(method{i,1}.Accuracy);
-    Specifity = mean(method{i,1}.Specifity);
-    Sensivity = mean(method{i,1}.Sensivity);
-    F1 = mean(method{i,1}.F1);
-    Recall = mean(method{i,1}.Recall);
+    TP = TP+(method{i,1}.NumberTP);
+    FP = FP+(method{i,1}.NumberFP);
+    FN = FN+(method{i,1}.NumberFN);
+    TN = TN+(method{i,1}.NumberTN);
+    Precision = Precision +(method{i,1}.Precision);
+    Accuracy = Accuracy +(method{i,1}.Accuracy);
+    Specifity = Specifity+(method{i,1}.Specifity);
+    Sensivity = Sensivity +(method{i,1}.Sensivity);
+    F1 = F1+(method{i,1}.F1);
+    Recall = Recall+(method{i,1}.Recall);
     
-    Metodo = struct('NumberTP', TP, 'NumberFP', FP, 'NumberFN', FN, 'NumberTN', TN, 'Precision', Precision, 'Accuracy', Accuracy, 'Specifity', Specifity, 'Sensivity', Sensivity,'F1', F1,'Recall', Recall);
 
 end
+    TP = TP/(length(method));
+    FP = FP/(length(method));
+    FN = FN/(length(method));
+    TN = TN/(length(method));
+    Precision = Precision /(length(method));
+    Accuracy = Accuracy /(length(method));
+    Specifity = Specifity/(length(method));
+    Sensivity = Sensivity/(length(method));
+    F1 = F1/(length(method));
+    Recall = Recall/(length(method));
+    Metodo = struct('NumberTP', TP, 'NumberFP', FP, 'NumberFN', FN, 'NumberTN', TN, 'Precision', Precision, 'Accuracy', Accuracy, 'Specifity', Specifity, 'Sensivity', Sensivity,'F1', F1,'Recall', Recall);
+
 end
