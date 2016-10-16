677eeb71dcc5013bf0929a6e049526294d04f104
aRWorld90 <arnau.casadevall.saiz@gmail.com>
2016-10-16 19:04:04 +0200

    Update calculateAverage.m

--- week1/calculateAverage.m
+++ week1/calculateAverage.m
@@ -1,29 +1,35 @@
 function [Metodo]= calculateAverage(method)
-TP=0;
-FP=0;
-FN=0;
-TN=0;
-Precision=0;
-Accuracy=0;
-Sensivity=0;
-Specifity=0;
-F1=0;
-Recall=0;
-
-for i = 1:length(method)
-    TP = TP+(method{i,1}.NumberTP);
-    FP = FP+(method{i,1}.NumberFP);
-    FN = FN+(method{i,1}.NumberFN);
-    TN = TN+(method{i,1}.NumberTN);
-    Precision = Precision +(method{i,1}.Precision);
-    Accuracy = Accuracy +(method{i,1}.Accuracy);
-    Specifity = Specifity+(method{i,1}.Specifity);
-    Sensivity = Sensivity +(method{i,1}.Sensivity);
-    F1 = F1+(method{i,1}.F1);
-    Recall = Recall+(method{i,1}.Recall);
+    % calculateAverage
+    % Calculate the average for the different signals separeted
+    %
+    %    Parameter name      Value
+    %    --------------      -----
+    %    'method'           Struct Method
     
+    TP=0;
+    FP=0;
+    FN=0;
+    TN=0;
+    Precision=0;
+    Accuracy=0;
+    Sensivity=0;
+    Specifity=0;
+    F1=0;
+    Recall=0;
 
-end
+    for i = 1:length(method)
+        TP = TP+(method{i,1}.NumberTP);
+        FP = FP+(method{i,1}.NumberFP);
+        FN = FN+(method{i,1}.NumberFN);
+        TN = TN+(method{i,1}.NumberTN);
+        Precision = Precision +(method{i,1}.Precision);
+        Accuracy = Accuracy +(method{i,1}.Accuracy);
+        Specifity = Specifity+(method{i,1}.Specifity);
+        Sensivity = Sensivity +(method{i,1}.Sensivity);
+        F1 = F1+(method{i,1}.F1);
+        Recall = Recall+(method{i,1}.Recall);
+    end
+    
     TP = TP/(length(method));
     FP = FP/(length(method));
     FN = FN/(length(method));
