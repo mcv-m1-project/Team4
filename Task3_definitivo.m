ae8da0c7fadfa926ab726c6b5248b65a2c7cf7d4
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-16 12:05:39 +0200

    Update Task3_definitivo.m

--- Task3_definitivo.m
+++ Task3_definitivo.m
@@ -1,9 +1,23 @@
-dirname = '\Users\Adrian\Desktop\master\M1\Team4-master\train_split'; %C:\Users\Adrian\Desktop\master\M1\Team4-master\train_split
+dirname = 'train_split_validation'; %C:\Users\Adrian\Desktop\master\M1\Team4-master\train_split_validation
 
-createMask(sA, tA, [dirname '/A'], 'A','RGB');
-createMask(sB, tB, [dirname '/B'], 'B','RGB');
-createMask(sC, tC, [dirname '/C'], 'C','RGB');
-createMask(sD, tD, [dirname '/D'], 'D','RGB');
-createMask(sE, tE, [dirname '/E'], 'E','RGB');
-createMask(sF, tF, [dirname '/F'], 'F','RGB');
+%Cases RGB, HSV or Lab
+createMask_definitivo(sA, vA, [dirname '/A'], 'A','RGB');
+createMask_definitivo(sB, vB, [dirname '/B'], 'B','RGB');
+createMask_definitivo(sC, vC, [dirname '/C'], 'C','RGB');
+createMask_definitivo(sD, vD, [dirname '/D'], 'D','RGB');
+createMask_definitivo(sE, vE, [dirname '/E'], 'E','RGB');
+createMask_definitivo(sF, vF, [dirname '/F'], 'F','RGB');
 
+createMask_definitivo(sA, vA, [dirname '/A'], 'A','HSV');
+createMask_definitivo(sB, vB, [dirname '/B'], 'B','HSV');
+createMask_definitivo(sC, vC, [dirname '/C'], 'C','HSV');
+createMask_definitivo(sD, vD, [dirname '/D'], 'D','HSV');
+createMask_definitivo(sE, vE, [dirname '/E'], 'E','HSV');
+createMask_definitivo(sF, vF, [dirname '/F'], 'F','HSV');
+
+createMask_definitivo(sA, vA, [dirname '/A'], 'A','Lab');
+createMask_definitivo(sB, vB, [dirname '/B'], 'B','Lab');
+createMask_definitivo(sC, vC, [dirname '/C'], 'C','Lab');
+createMask_definitivo(sD, vD, [dirname '/D'], 'D','Lab');
+createMask_definitivo(sE, vE, [dirname '/E'], 'E','Lab');
+createMask_definitivo(sF, vF, [dirname '/F'], 'F','Lab');
