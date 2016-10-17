b8fea250b00b1a6a95b6c2d64ddd2a82fd5b2835
aRWorld90 <arnau.casadevall.saiz@gmail.com>
2016-10-18 01:29:15 +0200

    New getParameters Week2/Task4

--- week2/Task4.m
+++ week2/Task4.m
@@ -1,1 +1,10 @@
-% Task 4
+%% -------------------  Task 4  -------------------  %%
+clc;
+clear all;
+close all;
+                                  
+dirname = '../train';
+txtFiles = dir(fullfile([dirname '/gt'],'*.txt')); % Get all .txt files
+maskFiles = dir(fullfile([dirname '/mask'],'*.png')); % Get all .png files
+
+[sABC, sDF, sE] = getParameters(txtFiles, maskFiles, dirname);
