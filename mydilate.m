28e6fb38426ba1a5de16b0b3e650b188c4606d2c
tpereztorres <tonipereztorres@gmail.com>
2016-10-18 18:30:26 +0200

    Add files via upload

--- mydilate.m
+++ mydilate.m
@@ -1,6 +1,5 @@
 function [dilation] = mydilate(im,se)
 
-% mask = [zeros(size(z,1),1) mask zeros(size(z,1),1)];
 rows = floor(size(se,1)/2);
 cols = floor(size(se,2)/2);
 
@@ -17,8 +16,6 @@ for i=1:size(mask,1)-(2*rows)
 end
 end
 
-imshow(dilation);
-
 % for i = 1:size(se,1):size(mask,1)
 %     for j = 1:size(se,2):size(mask,2)
 %         dilation(i:i+(size(se,1)-1),j:j+(size(se,2)-1)) = and(mask(i:i+(size(se,1)-1),j:j+(size(se,2)-1)),se);
@@ -27,20 +24,3 @@ imshow(dilation);
 %         end     
 %     end
 % end
-
-end
-
-A=im2bw(A);
-%Structuring element
-B2=getnhood(strel('line',7,90));
-m=floor(size(B2,1)/2);
-n=floor(size(B2,2)/2);
-%Pad array on all the sides
-C=padarray(A,[m n]);
-D=false(size(mask));
-for i=1:size(C,1)-(2*m)
-    for j=1:size(C,2)-(2*n)
-        Temp=C(i:i+(2*m),j:j+(2*n));
-        D(i,j)=max(max(Temp&B2));
-    end
-end
