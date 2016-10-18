f9c04692afe3b242dedd4bef49c86126c74430e8
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-18 20:47:37 +0200

    Add files via upload

---
+++ mydilategray.m
@@ -0,0 +1,29 @@
+function [dilation] = mydilate(im,se)
+
+rows = floor(size(se,1)/2);
+cols = floor(size(se,2)/2);
+
+%Pad array on all the sides
+mask = padarray(im,[rows cols]);
+
+
+
+for i=1:size(mask,1)-(2*rows)
+    for j=1:size(mask,2)-(2*cols)
+        temp = mask(i:i+(2*rows),j:j+(2*cols));
+        
+        for k=1:size(se,1)
+            for o=1:size(se,2)
+                if se(k,o)==1
+                
+                else
+                 temp(k,o)=0;
+                end
+            end
+        end
+        a=max(max(temp(:)));
+        dilation(i,j)=a;
+        
+    end
+end
+end
