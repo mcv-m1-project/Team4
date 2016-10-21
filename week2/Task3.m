596db34502df8d8ac9e882afa4914a50c14b1b08
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-21 17:29:31 +0200

    Update Task3.m

--- week2/Task3.m
+++ week2/Task3.m
@@ -1,44 +1,40 @@
 %BW2 = bwareaopen(BW,P) removes all connected components (objects) that have fewer than P pixels 
 %from the binary image BW, producing another binary image, BW2. The default connectivity is 8 
 %for two dimensions, 26 for three dimensions, and conndef(ndims(BW), 'maximal') for higher dimensions. This operation is known as an area opening.
+dirname = 'test';
+imagefilesA = dir(fullfile([dirname '/A'],'*.png')); % Get all .png files
+imagefilesB = dir(fullfile([dirname '/B'],'*.png'))
+imagefilesC = dir(fullfile([dirname '/C'],'*.png'))
+imagefilesD = dir(fullfile([dirname '/D'],'*.png'))
+imagefilesE = dir(fullfile([dirname '/E'],'*.png'))
+imagefilesF = dir(fullfile([dirname '/F'],'*.png'))
 
-%^No se si se puede utilizar esto iria bien como ultimo paso para quitar
-%elementos conexos que son tan pequeños que no pueden ser señales
+%dirname_new='test';
+%imagefilesmask = dir(fullfile([dirname_new],'*.png'));
 
-%https://es.mathworks.com/help/images/morphological-filtering.html
+dirname_new=[dirname '/mejora/A'];
+[s, mess, messid] = mkdir(dirname_new);
+Mejora (imagefilesA, [dirname '/A'], [dirname_new]);
 
-%La idea que se me ha ocurrido ha sido:
+dirname_new=[dirname '/mejora/B'];
+[s, mess, messid] = mkdir(dirname_new);
+Mejora (imagefilesB, [dirname '/B'], [dirname_new]);
 
-%1o dilatar para hacer la señal conexa con un operador pequeño (hay que
-%ajustar thresholds del hsv)
+dirname_new=[dirname '/mejora/C'];
+[s, mess, messid] = mkdir(dirname_new);
+Mejora (imagefilesC, [dirname '/C'], [dirname_new]);
 
-%2o Hacer un imfill para rellenar la señal (tiene que ser conexa de ahi lo
-%1o)
+dirname_new=[dirname '/mejora/D'];
+[s, mess, messid] = mkdir(dirname_new);
+Mejora (imagefilesD, [dirname '/D'], [dirname_new]);
 
-%3o Hacer un imopen para quitar el ruido con un operador mas grande ya que
-%hemos dilatado antes
-Im1 = imread('mask.01.001777.HSV.png');
-figure(1)
-imshow(Im1);
+dirname_new=[dirname '/mejora/E'];
+[s, mess, messid] = mkdir(dirname_new);
+Mejora (imagefilesE, [dirname '/E'], [dirname_new]);
 
-se1 = ones(5,5);
-se2 = ones(8,8);
-% Im1 = imfill(Image_mask);
-% figure(2)
-% imshow(Im1);
-
-Im2 = imdilate(Im1,se1);
-figure(2)
-imshow(Im2);
-
-Im3 = imfill(Im2,'holes');
-figure(3)
-imshow(Im3);
-
-Im4 = imopen(Im3,se2);
-figure(4)
-imshow(Im4);
-
+dirname_new=[dirname '/mejora/F'];
+[s, mess, messid] = mkdir(dirname_new);
+Mejora (imagefilesF, [dirname '/F'], [dirname_new]);
 
 % Im3 = imtophat(Im2,se);
 % figure(2)
