f8a4ffa3919ae6c2c9202a1e8a36b0cd3150021d
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-22 14:32:49 +0200

    Update Task4.m

--- week2/Task4.m
+++ week2/Task4.m
@@ -14,9 +14,9 @@ Histogram2D_ABC = createHistogram(sABC, dirname, 'ABC',nbins);
 Histogram2D_DF = createHistogram(sDF, dirname, 'DF',nbins);
 Histogram2D_E = createHistogram(sE, dirname, 'E_img',nbins);
 
-HistogramBackProjection(Histogram2D_ABC,dirname,nbins,'ABC')
-HistogramBackProjection(Histogram2D_DF,dirname,nbins,'DF')
-HistogramBackProjection(Histogram2D_E,dirname,nbins,'E')
+HistogramBackProjection(sABC,[dirname '/'],nbins,'ABC',Histogram2D_ABC)
+HistogramBackProjection(sDF,[dirname '/'],nbins,'DF',Histogram2D_DF)
+HistogramBackProjection(sE,[dirname '/'],nbins,'E',Histogram2D_E)
 
 % Histogram Backprojection
 %figure;
