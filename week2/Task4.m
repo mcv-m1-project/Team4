a4f3968c12ae5270f2abaf33ee7436e8850ac959
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-22 14:01:48 +0200

    Update Task4.m

--- week2/Task4.m
+++ week2/Task4.m
@@ -3,25 +3,25 @@ clc;
 clear all;
 close all;
                                   
-dirname = '/Users/Toni/Desktop/Toni/Master/M1/ProjectMaterials/Week 1/Code/train';
+dirname = 'train';
 txtFiles = dir(fullfile([dirname '/gt'],'*.txt')); % Get all .txt files
 maskFiles = dir(fullfile([dirname '/mask'],'*.png')); % Get all .png files
+nbins=[100,100];
 
 [sABC, sDF, sE] = getParameters(txtFiles, maskFiles, dirname);
 
-Histogram2D_ABC = createHistogram(sABC, dirname, 'ABC');
-Histogram2D_DF = createHistogram(sDF, dirname, 'DF');
-Histogram2D_E = createHistogram(sE, dirname, 'E_img');
+Histogram2D_ABC = createHistogram(sABC, dirname, 'ABC',nbins);
+Histogram2D_DF = createHistogram(sDF, dirname, 'DF',nbins);
+Histogram2D_E = createHistogram(sE, dirname, 'E_img',nbins);
 
-= HistogramBackProjection(struct,dirname,dirname_new)
-= HistogramBackProjection(struct,dirname,dirname_new)
-= HistogramBackProjection(struct,dirname,dirname_new)
+HistogramBackProjection(Histogram2D_ABC,dirname,nbins,'ABC')
+HistogramBackProjection(Histogram2D_DF,dirname,nbins,'DF')
+HistogramBackProjection(Histogram2D_E,dirname,nbins,'E')
 
 % Histogram Backprojection
-figure;
-bar3(Histogram2D);
+%figure;
+%bar3(Histogram2D);
 
-Histogram2D = Histogram2D/sum(Histogram2D);
-figure;
-bar3(Histogram2D);
-
+%Histogram2D = Histogram2D/sum(Histogram2D);
+%figure;
+%bar3(Histogram2D);
