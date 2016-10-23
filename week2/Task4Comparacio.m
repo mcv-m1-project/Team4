553a4a35147b428c519855e75daf7e2e7d27fcd8
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-23 13:00:36 +0200

    Add files via upload

---
+++ week2/Task4Comparacio.m
@@ -0,0 +1,46 @@
+dirname = 'test';
+dirname_new = 'test/Masks';
+sA = dir(fullfile([dirname '/Masks/ABC'],'*.png')); % Get all .png files
+sB = dir(fullfile([dirname '/Masks/DF'],'*.png'));
+sC = dir(fullfile([dirname 'Masks/E_img'],'*.png'));
+
+
+
+Metodo_HSV_ABC = TesteigMask(sA,[dirname '/A'],[dirname_new '/ABC'],'HSV');
+
+Metodo_HSV_DF = TesteigMask(sB,[dirname '/B'],[dirname_new '/DF'],'HSV');
+
+Metodo_HSV_E = TesteigMask(sC,[dirname '/C'],[dirname_new '/E'],'HSV');
+
+
+
+% RGB
+% Calculate the average for the different signals separeted 
+    
+% HSV
+HSV_ABC_avg = calculateAverage(Metodo_HSV_ABC);
+HSV_DF_avg = calculateAverage(Metodo_HSV_DF);
+HSV_E_avg = calculateAverage(Metodo_HSV_E);
+
+
+HSV_TP = HSV_ABC_avg.NumberTP + HSV_DF_avg.NumberTP + HSV_E_avg.NumberTP;
+HSV_TP = HSV_TP/3;
+HSV_FP = HSV_ABC_avg.NumberFP + HSV_DF_avg.NumberFP + HSV_E_avg.NumberFP;
+HSV_FP = HSV_FP/3;
+HSV_FN = HSV_ABC_avg.NumberFN + HSV_DF_avg.NumberFN + HSV_E_avg.NumberFN;
+HSV_FN = HSV_FN/3;
+HSV_TN = HSV_ABC_avg.NumberTN + HSV_DF_avg.NumberTN + HSV_E_avg.NumberTN;
+HSV_TN = HSV_TN/3;
+HSV_Precision = [HSV_ABC_avg.Precision, HSV_DF_avg.Precision, HSV_E_avg.Precision];
+HSV_Precision = mean(HSV_Precision(:));
+HSV_Accuracy = [HSV_ABC_avg.Accuracy, HSV_DF_avg.Accuracy, HSV_E_avg.Accuracy];
+HSV_Accuracy = mean(HSV_Accuracy(:));
+HSV_Specifity = [HSV_ABC_avg.Specifity, HSV_DF_avg.Specifity, HSV_E_avg.Specifity];
+HSV_Specifity = mean(HSV_Specifity(:));
+HSV_Sensivity = [HSV_ABC_avg.Sensivity, HSV_DF_avg.Sensivity, HSV_E_avg.Sensivity];
+HSV_Sensivity = mean(HSV_Sensivity(:));
+HSV_F1 = [HSV_ABC_avg.F1, HSV_DF_avg.F1, HSV_E_avg.F1];
+HSV_F1 = mean(HSV_F1(:));
+HSV_Recall = [HSV_ABC_avg.Recall, HSV_DF_avg.Recall, HSV_E_avg.Recall];
+HSV_Recall = mean(HSV_Recall(:));
+
