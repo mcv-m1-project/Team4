79014be94a97480b19a1d74f5f5dd9e6842daa09
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-23 12:59:56 +0200

    Update Task3.m

--- week2/Task3.m
+++ week2/Task3.m
@@ -1,6 +1,3 @@
-%BW2 = bwareaopen(BW,P) removes all connected components (objects) that have fewer than P pixels 
-%from the binary image BW, producing another binary image, BW2. The default connectivity is 8 
-%for two dimensions, 26 for three dimensions, and conndef(ndims(BW), 'maximal') for higher dimensions. This operation is known as an area opening.
 dirname = 'test';
 imagefilesA = dir(fullfile([dirname '/A'],'*.png')); % Get all .png files
 imagefilesB = dir(fullfile([dirname '/B'],'*.png'))
@@ -9,8 +6,8 @@ imagefilesD = dir(fullfile([dirname '/D'],'*.png'))
 imagefilesE = dir(fullfile([dirname '/E'],'*.png'))
 imagefilesF = dir(fullfile([dirname '/F'],'*.png'))
 
-%dirname_new='test';
-%imagefilesmask = dir(fullfile([dirname_new],'*.png'));
+dirname_new='test';
+imagefilesmask = dir(fullfile([dirname_new],'*.png'));
 
 dirname_new=[dirname '/mejora/A'];
 [s, mess, messid] = mkdir(dirname_new);
@@ -36,11 +33,20 @@ dirname_new=[dirname '/mejora/F'];
 [s, mess, messid] = mkdir(dirname_new);
 Mejora (imagefilesF, [dirname '/F'], [dirname_new]);
 
-% Im3 = imtophat(Im2,se);
-% figure(2)
-% imshow(Im3);
 
-% Im4 = imbothat(Im2,se);
-% figure(3)
-% imshow(Im4);
+%---------------------------------------------------------------------------------------------------------------
+%2nd part
+%Realization of the masks for the validation test of the virtual server!
+
+%dirname='test/test/masks';
+
+
+%imagefilestest = dir(fullfile([dirname],'*.png'));
+
+%dirname_new=[dirname '/mejora'];
+%[s, mess, messid] = mkdir(dirname_new);
+%Mejora (imagefilestest, [dirname], [dirname_new]);
+
+
+
 
