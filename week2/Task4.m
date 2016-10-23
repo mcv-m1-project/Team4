65422d886167949066b2ef056652b9e551a444f1
SCLSCL <sergio.castro.latorre@gmail.com>
2016-10-23 16:03:06 +0200

    Update Task4.m

--- week2/Task4.m
+++ week2/Task4.m
@@ -1,4 +1,4 @@
-%% -------------------  Task 4 (Week2) -------------------  %%
+%% -------------------  Task 4 (Week2) -------------------  %% part1
 clc;
 clear all;
 close all;
@@ -19,16 +19,40 @@ Histogram2D_ABC(1,1)= 0;
 Histogram2D_DF(1,1)= 0;
 Histogram2D_E(1,1)= 0;
 
-%%
+
 
 HistogramBackProjection(sABC,[dirname '/'],nbins,'ABC',Histogram2D_ABC)
 HistogramBackProjection(sDF,[dirname '/'],nbins,'DF',Histogram2D_DF)
 HistogramBackProjection(sE,[dirname '/'],nbins,'E',Histogram2D_E)
 
-% Histogram Backprojection
-%figure;
-%bar3(Histogram2D);
+%%
+%---------------------------------------------------------------------------------------------------------------
+%2nd part
+%Realization of the masks for the validation test of the virtual server!
 
-%Histogram2D = Histogram2D/sum(Histogram2D);
-%figure;
-%bar3(Histogram2D);
+%clc;
+%clear all;
+%close all;
+                                  
+%dirname = 'train';
+%txtFiles = dir(fullfile([dirname '/gt'],'*.txt')); % Get all .txt files
+%maskFiles = dir(fullfile([dirname '/mask'],'*.png')); % Get all .png files
+%nbins=[100,100];
+
+%[sABC, sDF, sE] = getParameters(txtFiles, maskFiles, dirname);
+
+%Histogram2D_ABC = createHistogram(sABC, dirname, 'ABC',nbins);
+%Histogram2D_DF = createHistogram(sDF, dirname, 'DF',nbins);
+%Histogram2D_E = createHistogram(sE, dirname, 'E_img',nbins);
+
+%Histogram2D_ABC(1,1)= 0;
+%Histogram2D_DF(1,1)= 0;
+%Histogram2D_E(1,1)= 0;
+
+%dirname='test/test/Images';
+%imagefilestest = dir(fullfile([dirname],'*.jpg'));
+
+%HistogramBackProjectionTest(imagefilestest,[dirname '/'],nbins,'ABC',Histogram2D_ABC)
+%HistogramBackProjectionTest(imagefilestest,[dirname '/'],nbins,'DF',Histogram2D_DF)
+%HistogramBackProjectionTest(imagefilestest,[dirname '/'],nbins,'E',Histogram2D_E)
+
