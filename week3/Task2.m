9e30d7aaf647adde48a83cb5136d8683ff9a98c1
tpereztorres <tonipereztorres@gmail.com>
2016-10-25 18:41:04 +0200

    Add files via upload

---
+++ week3/Task2.m
@@ -0,0 +1,15 @@
+im = imread('mejora/A/mask.01.001796.HSV.png'); % read the image
+im_width = size(im,2);
+im_height = size(im,1);
+
+w_width = 32;
+w_height = 32;
+
+window = zeros(im_height,im_width);
+
+for i = 1:(im_height - w_height)
+    for j = 1:(im_width - w_width)
+        window(i,j) = im(i:(i + w_height - 1), j:(j + w_width - 1));
+        % if sum(window) > 
+    end
+end
