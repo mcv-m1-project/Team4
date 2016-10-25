f1cde867e80cb1b029a2d060c782335590359b98
tpereztorres <tonipereztorres@gmail.com>
2016-10-25 23:18:01 +0200

    Add files via upload

--- week3/Task2.m
+++ week3/Task2.m
@@ -5,11 +5,18 @@ im_height = size(im,1);
 w_width = 32;
 w_height = 32;
 
-window = zeros(im_height,im_width);
+window = true(im_height,im_width);
 
 for i = 1:(im_height - w_height)
     for j = 1:(im_width - w_width)
-        window(i,j) = im(i:(i + w_height - 1), j:(j + w_width - 1));
-        % if sum(window) > 
+        window(i:i + (w_height - 1),j:j + (w_width - 1)) = im(i:i + (w_height - 1), j:j + (w_width - 1));
+            if sum(window) >  
+                
+                
+            end
     end
 end
+
+imshow(im);
+
+imshow(window);
