e011ea8748e66a089f3b84048ca0fd18b2d82f61
tpereztorres <tonipereztorres@gmail.com>
2016-10-25 17:53:36 +0200

    Add files via upload

--- week3/Task1.m
+++ week3/Task1.m
@@ -1,17 +1,22 @@
-im = imread('mask.01.001796.HSV.png');
-CC = bwconncomp(im);
-nobj = CC.NumObjects;
-stats = regionprops(CC,'BoundingBox');
+im = imread('mejora/A/mask.01.001796.HSV.png'); % read the image
 
-boundingboxes = cat(1,stats.BoundingBox);
+CC = bwconncomp(im); % find all the connected components in the mask
+
+nobj = CC.NumObjects; % choose the variable NumObjects in the struct to know 
+% how many objects there are 
+
+stats = regionprops(CC,'BoundingBox'); % measure a set of properties for each
+% connected component
+
+boundingboxes = cat(1,stats.BoundingBox); % separate the 4 properties inside stats 
 
 %Filtering part
-
-%We will filter the small objects, the big objecta and the objects with
-%incorrect filling ratio and aspect ratio
+%We will filter the small objects, the big objects and the objects with
+%incorrect filling ratio and aspect ratio:
 %filling ratio in triangles = 1/2
 %filling ratio in circle = pi/4
 %Correct aspect ratio = 1:1 
+
 d = 1;
 for k = 1:size(boundingboxes,1)
     ratio = (boundingboxes(k,3)/(boundingboxes(k,4)));
@@ -23,7 +28,6 @@ end
 
 boundingboxes(delete,:)=[]; %We eliminate the objects that can't be a signal 
 
-
 imshow(im); %Plot the image and the boxes
 hold on
 for k = 1:size(boundingboxes,1)
@@ -31,9 +35,7 @@ for k = 1:size(boundingboxes,1)
 end
 hold off
 
-
 CCboxes = struct( 'x', boundingboxes(:,1), ... 
                'y', boundingboxes(:,2), ... 
                'width', boundingboxes(:,3), ... 
                'height', boundingboxes(:,4) );  
-              
