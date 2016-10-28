b8e976e76f9f40494904a34f86c8e1927a122eb1
tpereztorres <tonipereztorres@gmail.com>
2016-10-28 21:06:48 +0200

    Add files via upload

--- week3/Task2.m
+++ week3/Task2.m
@@ -3,55 +3,23 @@ clear all
 clc
 
 dirname = 'train';
-txtFiles = dir(fullfile([dirname '/gt'],'*.txt')); % get txt files
-
-% read it to measure the coordinates, width, height and the bounding box
-for k = 1:length(txtFiles)
-    j = 1; % Index to count number of each signal type
-    a = 0; % Test Debug
-
-    fileTxt = fullfile([dirname '/gt'], txtFiles(k).name); % File by File
-    fileTxtID = fopen(fileTxt);
-    dataTxt{k,1} = textscan(fileTxtID,'%f %f %f %f %s');
-    
-    tly{k,1}(j) = dataTxt{k}{1}(j); % save minY          
-    tlx{k,1}(j) = dataTxt{k}{2}(j); % save minX
-    bry{k,1}(j) = dataTxt{k}{3}(j); % save maxY  
-    brx{k,1}(j) = dataTxt{k}{4}(j); % save maxX
-    width{k,1}(j) = abs(brx{k,1}(j) - tlx{k,1}(j)); % widht of each Ground Truth (maxX-minX)
-    height{k,1}(j) = abs(bry{k,1}(j) - tly{k,1}(j)); % height of each Ground Truth (maxY-minY)
-    bbox{k,1}(j) = width{k,1}(j) .* height{k,1}(j);             
-
-    fclose all;                        
-end
-
-% calculate the mean of width
-w_bbox = 0;
-for i = 1:length(width)
-    w_bbox = w_bbox + width{i,1};
-end
-w_bbox = round(w_bbox/length(width));
-
-% calculate the mean of height
-h_bbox = 0;
-for i = 1:length(height)
-    h_bbox = h_bbox + height{i,1};
-end
-h_bbox = round(h_bbox/length(height)); 
-
-w = [w_bbox h_bbox];
-w = round(mean(w)); % final window
-
-im = imread('mejora/A/mask.01.001796.HSV.png'); % read the image
-im_width = size(im,2);
-im_height = size(im,1);
+w = getWindowSize(dirname);
 
 w_width = w;
 w_height = w;
 
-%window = zeros(w_height,w_width);
+window = zeros(w_height,w_width);
 window_area = w_height*w_width;
-w_signal = {};
+dirname1 = 'mejora';
+maskFiles = dir(fullfile(dirname1,'*HSV.png')); % Get all .png files
+w_position = cell(length(maskFiles),1);
+
+for k = 1:length(maskFiles)
+
+im = imread(fullfile(dirname1,maskFiles(k).name)); % read the image
+im_width = size(im,2);
+im_height = size(im,1);
+
 d = 1;
 for i = 1:(im_height - w_height)
     for j = 1:(im_width - w_width)
@@ -61,30 +29,41 @@ for i = 1:(im_height - w_height)
         fr = signal_area / window_area;
         %if fr > 0.5
         if (fr > 0.4 && fr < 0.6) || (fr > 0.7 && fr < 0.85) || (fr > 0.9 && fr <= 1)
-            w_signal{d} = window;
-            w_position(d,:) = [j,i,w_height,w_width];
+            % w_signal{d} = window;
+            w_position{k}(d,:) = [j,i,w_height,w_width];
             d = d + 1;
         end
     end
 end
 
-dd = 1;
-delete = 0;
-for i = 1:(length(w_position)-1)
-    if w_position(i,2) == w_position((i+1),2)
-        delete(dd) = i;
-        dd = dd + 1;  
-    end
+if (~isempty(w_position{k}))
+    [unic, ia] = unique(w_position{k}(:,1));
+    % freq = [unic,histc(w_position(:,1),unic)];
+    w_position{k} = w_position{k}(ia,:);
+    w_position{k} = w_position{k}(1:10:end,:);
+end
 end
 
-w_position(delete,:) = []; % delete boxes
+% dd = 1;
+% delete = 0;
+% for i = 1:(length(w_position) - 1)
+%     if w_position(i,2) == w_position((i+1),2) % || (w_position(i,2) <= (w_position((i+1),2) + 30))  
+%         delete(dd) = i;
+%         dd = dd + 1;  
+%     end
+% end
+
+% w_position(delete,:) = []; % delete boxes
 
 %% Plot of the image and all the boxes
 
-imshow(im); %Plot the image and the boxes
-hold on
-for k = 1:size(w_position,1)
-    rectangle('position',w_position(k,:),'Edgecolor','g')
+for k = 1:length(maskFiles)
+    imshow(imread(fullfile(dirname1,maskFiles(k).name))); %Plot the image and the boxes
+    hold on
+    for d = 1:length(w_position{k,1})
+    rectangle('position',w_position{k}(d,:),'Edgecolor','g')
+    end
+    pause();
 end
 
 %% Plot of the image and only one box
