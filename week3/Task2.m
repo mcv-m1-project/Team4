3acc08f4b5662d9bf35beb77fa9e3d3b00824a77
aRWorld90 <arnau.casadevall.saiz@gmail.com>
2016-10-30 19:45:15 +0100

    Update Task2.m

--- week3/Task2.m
+++ week3/Task2.m
@@ -5,6 +5,8 @@ clear all
 clc
 
 dirTxt = '../train';
+dirMask = '../mejora';
+
 w = getWindowSize(dirTxt); % Get Optimal Width and Height using TxtFiles
 
 w_width = w;
@@ -12,12 +14,11 @@ w_height = w;
 
 window = zeros(w_height,w_width);
 window_area = w_height*w_width;
-dirMask = '../mejora';
+
 maskFiles = dir(fullfile(dirMask,'*HSV.png')); % Get all .png files
-w_position = cell(length(maskFiles),1);
 
 tic
-for k = 1:3 %length(maskFiles)
+for k = 1:length(maskFiles)
     w_pos = [0, 0, 0, 0];
     im = imread(fullfile(dirMask,maskFiles(k).name)); % read the image
     im_width = size(im,2);
@@ -28,13 +29,9 @@ for k = 1:3 %length(maskFiles)
         for j = 1:(im_width - w_width)
             window = im(i:i + (w_height - 1), j:j + (w_width - 1));
             signal_area = sum(sum(window));
-            %bbox_area = boundingboxes(k,3).*(boundingboxes(k,4));
             fr = signal_area / window_area;
-            %if fr > 0.5
 
             if (fr > 0.4 && fr < 0.6) || (fr > 0.7 && fr < 0.85) || (fr > 0.9 && fr <= 1)
-                % w_signal{d} = window;
-                w_position{k}(d,:) = [j,i,w_height,w_width];
                 w_pos(d,:) = [j,i,w_width,w_height];
                 d = d + 1;
             end
@@ -56,34 +53,19 @@ for k = 1:3 %length(maskFiles)
         w_pos = w_position{2};
         w_pos = w_pos(ia,:);
         w_pos = w_pos(1:10:end,:);
-    end
-    
+    end    
     k
     
     windowCandidates(k,:) = struct('x', w_pos(:,1), 'y' , w_pos(:,2), 'w', w_pos(:,3), 'h', w_pos(:,4));
     clear w_pos
-
-% 
+    
 end
 timeTask2 = toc;
-% dd = 1;
-% delete = 0;
-% for i = 1:(length(w_position) - 1)
-%     if w_position(i,2) == w_position((i+1),2) % || (w_position(i,2) <= (w_position((i+1),2) + 30))  
-%         delete(dd) = i;
-%         dd = dd + 1;  
-%     end
-% end
 
-% w_position(delete,:) = []; % delete boxes
-
-%% Result: List Of Bounding Boxes containing a detection
-
-% save as .mat file
+%% Save as .mat File
 save windowCandidates
 
 %% Plot of the image and all the boxes
-
 % for k = 1:length(maskFiles)
 %     imshow(imread(fullfile(dirname1,maskFiles(k).name)));
 %     hold on
@@ -92,9 +74,6 @@ save windowCandidates
 %     end
 %     pause();
 % end
-% 
-% %% Plot of the image and only one box
-% 
 % imshow(im);
 % hold on
 % rectangle('position',w_position(1,:),'Edgecolor','g')
