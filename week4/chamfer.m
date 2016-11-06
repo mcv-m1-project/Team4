12f8f8c482dbc1b93420226e719458946d6b6bd8
AdrianSalvador <adrisalva122@gmail.com>
2016-11-06 18:18:47 +0100

    Add files via upload

--- week4/chamfer.m
+++ week4/chamfer.m
@@ -2,13 +2,40 @@ function w_pos = chamfer(dirname,maskFiles,template_edge)
 % B -> maskFiles
 % T -> templatge_edge
 w_pos = [];
-
-CircleSizes = 200:260;
+load('template_bw.mat');
+%prepare circles
+CircleSizes = round(linspace(150,300,7));
 for p = 1:length(CircleSizes)
     circleedge{p} = CreateCircles(CircleSizes(p));
 end
 
-    for k = 2:10%length(maskFiles)
+%prepare up triangles
+temptriup = double(template_bw.upTri);
+
+for l = 1:7
+    sub = 0.3*l;
+    triupe{l} = imresize(temptriup,sub);
+    triupe{l} = edge(triupe{l},'Canny');
+end
+
+%prepare down triangles
+temptridown = double(template_bw.downTri);
+
+for l = 1:7
+    sub = 0.3*l;
+    tridowne{l} = imresize(temptridown,sub);
+    tridowne{l} = edge(tridowne{l},'Canny');
+end
+
+%prepare squares
+
+SquareSizes = round(linspace(150,300,7));
+for m = 1:length(SquareSizes)
+    squareedge{m} = CreateSquare(SquareSizes(m));
+end
+
+
+    for k = 2 %:10%length(maskFiles)
         %toSplit = strsplit(maskFiles(k).name,{mask.',''});
         mask = imread([dirname, '/', maskFiles(k).name]);
         %mask = imread(strjoin(['mejora/', 'mask.', toSplit(2), '.HSV.png'],''));
@@ -27,7 +54,19 @@ end
         %falta hacerlo para cada tamaño de template
         %DupTri = conv2(D,double(template_edge.upTri),'same');
         for p = 1:length(CircleSizes)
-        Dcircle{p} = conv2(D,double(circleedge{p}),'same');
+        Dcircle{p} = conv2(D,double(circleedge{p}),'valid');
+        end
+        
+        for p = 1:length(CircleSizes)
+        Dtriup{p} = conv2(D,double(triupe{p}),'valid');
+        end
+        
+        for p = 1:length(CircleSizes)
+        Dtridown{p} = conv2(D,double(tridowne{p}),'valid');
+        end
+        
+        for p = 1:length(CircleSizes)
+        Dsquare{p} = conv2(D,double(squareedge{p}),'valid');
         end
         
         
@@ -44,18 +83,109 @@ end
         %[T_height, T_width] = size(T);
         d = 1;
         %f=T(4363463);
-        for i=1:m_height %Here we will see the convolution result, the position with values near to 0, will be the
-             for j=1:m_width
-                 for p=1:length(CircleSizes)
-                    if (Dcircle{p}(i,j)<20) 
-                    w_pos{k}(d,:) = [j-size(Dcircle{p},1),i-size(Dcircle{p},1),size(Dcircle{p},1),size(Dcircle{p},1)];
-                    d = d + 1;
-                    end
-                
-                end
-            end
-        end
         
+       valmin = 999999999999999;
+       for p=1:length(CircleSizes) 
+       valcomp = min(min(Dcircle{p}));
+       if(valcomp<valmin)
+           [j,i]=find(Dcircle{p}==min(valcomp));
+           w_pos{k}(d,:) = [j,i,size(circleedge{p},1),size(circleedge{p},1)];
+           valmin = valcomp;
+       end
+       end
+       
+      
+       d = d + 1;
+      valmin = 999999999999999;
+       for p=1:length(CircleSizes) 
+       valcomp = min(min(Dtriup{p}));
+       if(valcomp<valmin)
+           [j,i]=find(Dtriup{p}==min(valcomp));
+           w_pos{k}(d,:) = [j,i,size(triupe{p},1),size(triupe{p},1)];
+           valmin = valcomp;
+       end
+       end 
+       
+       
+       d = d + 1;
+       
+       valmin = 999999999999999;
+       for p=1:length(CircleSizes) 
+       valcomp = min(min(Dtridown{p}));
+       if(valcomp<valmin)
+           [j,i]=find(Dtridown{p}==min(valcomp));
+           w_pos{k}(d,:) = [j,i,size(tridowne{p},1),size(tridowne{p},1)];
+           valmin = valcomp;
+       end
+       end 
+       
+       d = d + 1;
+       
+       valmin = 999999999999999;
+       for p=1:length(CircleSizes) 
+       valcomp = min(min(Dsquare{p}));
+       if(valcomp<valmin)
+           [j,i]=find(Dsquare{p}==min(valcomp));
+           w_pos{k}(d,:) = [j,i,size(squareedge{p},1),size(squareedge{p},1)];
+           valmin = valcomp;
+       end
+       end  
+        
+        
+ %f=D(4363463);       
+%         for i=1:m_height %Here we will see the convolution result, the position with values near to 0, will be the
+%              for j=1:m_width
+%                  for p=1:length(CircleSizes)
+%                     if (Dcircle{p}(i,j)<150) 
+%                         
+%                     w_pos{k}(d,:) = [j,i,size(Dcircle{p},1),size(Dcircle{p},1)];
+%                     d = d + 1;
+%                     end
+%                 
+%                 end
+%             end
+%         end
+       
+        
+%         for i=1:m_height %Here we will see the convolution result, the position with values near to 0, will be the
+%              for j=1:m_width
+%                  for p=1:length(CircleSizes)
+%                     if (Dtriup{p}(i,j)<150) 
+%                     w_pos{k}(d,:) = [j-size(Dtriup{p},1),i-size(Dtriup{p},1),size(Dtriup{p},1),size(Dtriup{p},1)];
+%                     d = d + 1;
+%                     end
+%                 
+%                 end
+%             end
+%         end
+%         
+%         for i=1:m_height %Here we will see the convolution result, the position with values near to 0, will be the
+%              for j=1:m_width
+%                  for p=1:length(CircleSizes)
+%                     if (Dtridown{p}(i,j)<150) 
+%                     w_pos{k}(d,:) = [j-size(Dtridown{p},1),i-size(Dtridown{p},1),size(Dtridown{p},1),size(Dtridown{p},1)];
+%                     d = d + 1;
+%                     end
+%                 
+%                 end
+%             end
+%         end
+%         
+%         for i=1:m_height %Here we will see the convolution result, the position with values near to 0, will be the
+%              for j=1:m_width
+%                  for p=1:length(CircleSizes)
+%                     if (Dsquare{p}(i,j)<150) 
+%                     w_pos{k}(d,:) = [j-size(Dsquare{p},1),i-size(Dsquare{p},1),size(Dsquare{p},1),size(Dsquare{p},1)];
+%                     d = d + 1;
+%                     end
+%                 
+%                 end
+%             end
+%         end
+%         
+%         
+% f=D(4363463);
+
 %         [m_height,m_width] = size(mask);
 %         T = single(template_edge.upTri);
 %         M = zeros(m_height,m_width);
@@ -84,8 +214,17 @@ end
 % multiplied by the pixel values in T
 % ? if T is an exact match to B at location (i,j) then M(i,j) = 0
 % ? if the edges in B are slightly displaced from their ideal locations in T, we
-% still get a good match using the distance transform technique        
+% still get a good match using the distance transform technique 
+
+imshow(ed);
+    
+hold on
+for j = 1:length(w_pos{k})
+    rectangle('position',[w_pos{k}(j,1), w_pos{k}(j,2), w_pos{k}(j,3), w_pos{k}(j,4)],'Edgecolor','g')
+end
+pause();
     end
+    
 end
 
 
