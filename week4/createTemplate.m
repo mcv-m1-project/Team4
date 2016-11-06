306f87e525812d8f978197b6fb677979c2084faa
tpereztorres <tonipereztorres@gmail.com>
2016-11-06 13:44:05 +0100

    Add files via upload

--- week4/createTemplate.m
+++ week4/createTemplate.m
@@ -1,20 +1,66 @@
-function croppedTemp = createTemplate(dirname, txtFiles, size, form_factor)
-
-    wh = round(sqrt(size.*form_factor));
-    croppedTemp = zeros([wh wh 3]);
+function [template_bw,template_color,template_gray,template_edge] = createTemplate(dirname, txtFiles, size, form_factor, prop)
+    
+    % [wh wh] to resize image 
+    whU = round(sqrt(size.upTri .* form_factor.upTri));
+    whD = round(sqrt(size.downTri .* form_factor.downTri));
+    whC = round(sqrt(size.circle .* form_factor.circle));
+    whS = round(sqrt(size.square .* form_factor.square));
+    
+    %croppedTemp = zeros([wh wh 3]);
+    %croppedTemp = 0;
     gt = getGroundTruth(dirname, txtFiles);
+    
+    up = 0; down = 0; circle = 0; sq = 0;
+    tempU = 0; tempD = 0; tempC = 0; tempS = 0;
+    tempiU = 0; tempiD = 0; tempiC = 0; tempiS = 0;
     
     for i = 1:length(txtFiles)
         toSplit = strsplit(txtFiles(i).name,{'gt.','.txt'});
-        
-        %fileTxt = fullfile([dirname '/gt'], strjoin(['gt.' toSplit(2) '.txt'],'')); % File by File
-        im = imread(strjoin([dirname, '/', toSplit(2), '.jpg'],''));
-        
+        fileTxt = fullfile([dirname '/gt'], strjoin(['gt.' toSplit(2) '.txt'],'')); % File by File
+        image = imread(strjoin([dirname, '/', toSplit(2), '.jpg'],''));
         mask = imread(strjoin([dirname, '/mask/mask.', toSplit(2), '.png'],''));
-        croppedMask = double(imcrop(im,[gt(i).x,gt(i).y,gt(i).w,gt(i).h]));
-        croppedMask = imresize(croppedMask,[wh wh]);
         
-        croppedTemp = croppedTemp + im;
+        [~, s] = LoadAnnotations(fileTxt);
+        
+        j = 1;
+        while j<=length(s)
+            croppedMask = double(imcrop(mask,[gt{i}(j).x, gt{i}(j).y, gt{i}(j).w, gt{i}(j).h]));
+            croppedIm = double(imcrop(image,[gt{i}(j).x, gt{i}(j).y, gt{i}(j).w, gt{i}(j).h]));
+            if s{j} == 'A' % upTri
+                m = imresize(croppedMask,[whU whU]);
+                im = imresize(croppedIm,[whU whU]);
+                tempU = tempU + m;
+                tempiU = tempiU + im;
+            elseif s{j} == 'B' % downTri
+                m = imresize(croppedMask,[whD whD]);
+                im = imresize(croppedIm,[whD whD]);
+                tempD = tempD + m;
+                tempiD = tempiD + im;
+            elseif s{j} == 'F' % Square
+                m = imresize(croppedMask,[whS whS]);
+                im = imresize(croppedIm,[whS whS]);
+                tempS = tempS + m;
+                tempiS = tempiS + im;
+            elseif s{j} == 'C' || s{j} == 'D' || s{j} == 'E' % C, D, E (Circle)
+                m = imresize(croppedMask,[whC whC]);
+                im = imresize(croppedIm,[whC whC]);
+                tempC = tempC + m;
+                tempiC = tempiC + im;
+            end
+       
+            j = j + 1;
+            
+        end
+        i
+       
     end
-
+    
+    template_bw = struct('upTri', round(uint8(tempU/length(prop.A))), 'downTri', round(uint8(tempD/length(prop.B))), 'circle', round(uint8(tempC/(length(prop.C) + length(prop.D) + length(prop.E)))), 'square', round(uint8(tempS/length(prop.F))));
+    save ('template_bw.mat', 'template_bw'); 
+    template_color = struct('upTri', round(uint8(tempiU/length(prop.A))), 'downTri', round(uint8(tempiD/length(prop.B))), 'circle', round(uint8(tempiC/(length(prop.C) + length(prop.D) + length(prop.E)))), 'square', round(uint8(tempiS/length(prop.F))));
+    save ('template_color.mat', 'template_color'); 
+    template_gray = struct('upTri', rgb2gray(round(uint8(tempiU/length(prop.A)))), 'downTri', rgb2gray(round(uint8(tempiD/length(prop.B)))), 'circle', rgb2gray(round(uint8(tempiC/(length(prop.C) + length(prop.D) + length(prop.E))))), 'square', rgb2gray(round(uint8(tempiS/length(prop.F)))));
+    save ('template_gray.mat', 'template_gray'); 
+    template_edge = struct('upTri', edge(round(uint8(tempU/length(prop.A))),'Canny'), 'downTri', edge(round(uint8(tempD/length(prop.B))),'Canny'), 'circle', edge(round(uint8(tempC/(length(prop.C) + length(prop.D) + length(prop.E)))),'Canny'), 'square', edge(round(uint8(tempS/length(prop.F))),'Canny'));
+    save ('template_edge.mat', 'template_edge'); 
 end
--- week4/getProperties.m
+++ week4/getProperties.m
@@ -1,31 +1,25 @@
-function [propA, propB, propC, propD, propE, propF] = getProperties(dirname,txtFiles)
+function prop = getProperties(dirname,txtFiles)
     
-    disp('Getting Originals Ground Truth ...');
+    disp('Getting Properties ...');
     kA = 0; kB = 0; kC = 0; kD = 0; kE = 0; kF = 0; % k Index to each signal type
-
     
     for k = 1:length(txtFiles)
         j = 1; % Index to count number of each signal type
 
         fileTxtID = fopen([dirname '/gt/' txtFiles(k).name]);
-        dataTxt{k,1} = textscan(fileTxtID,'%f %f %f %f %s');
-        size = length(dataTxt{k}{1}); % Number of signals for each txt file
-
+        dataTxt = textscan(fileTxtID,'%f %f %f %f %s');
+        size = length(dataTxt{1}); % Number of signals for each txt file
+        
         while j <= size
-            tly{k,1}(j) = dataTxt{k}{1}(j); % save minY          
-            tlx{k,1}(j) = dataTxt{k}{2}(j); % save minX
-            bry{k,1}(j) = dataTxt{k}{3}(j); % save maxY  
-            brx{k,1}(j) = dataTxt{k}{4}(j); % save maxX
-            type{k,1}(j) = dataTxt{k}{5}(j); % save signal type
+            tly{k,1}(j) = dataTxt{1}(j); % save minY          
+            tlx{k,1}(j) = dataTxt{2}(j); % save minX
+            bry{k,1}(j) = dataTxt{3}(j); % save maxY  
+            brx{k,1}(j) = dataTxt{4}(j); % save maxX
+            type{k,1}(j) = dataTxt{5}(j); % save signal type
             width{k,1}(j) = abs(brx{k,1}(j) - tlx{k,1}(j)); % widht of each Ground Truth (maxX-minX)
             height{k,1}(j) = abs(bry{k,1}(j) - tly{k,1}(j)); % height of each Ground Truth (maxY-minY)
-            j=j+1;
-        end
-
-        fclose all;
-        
-        j = 1;
-        if type{k,1}{j} == 'A'
+            
+            if type{k,1}{j} == 'A'
                 kA = kA + 1;
                 propA(kA,:) = [width{k,1}(j) .* height{k,1}(j), width{k,1}(j) ./ height{k,1}(j)]; % Form Factor (Width/Height)
             elseif type{k,1}{j} == 'B'
@@ -44,7 +38,14 @@ function [propA, propB, propC, propD, propE, propF] = getProperties(dirname,txtF
                 kF = kF +1;
                 propF(kF,:) = [width{k,1}(j) .* height{k,1}(j), width{k,1}(j) ./ height{k,1}(j)]; % Form Factor (Width/Height)
             end
+
+            j = j + 1;
+        end
+        
+        fclose all;
     end
     
+    prop = struct('A', propA, 'B', propB, 'C', propC, 'D', propD, 'E', propE, 'F', propF);
+
     disp('Done.');
 end
