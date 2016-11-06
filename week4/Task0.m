be2e2f74275ba80c07508a0367c1f8114b165827
tpereztorres <tonipereztorres@gmail.com>
2016-11-06 19:10:54 +0100

    Add files via upload

--- week4/Task0.m
+++ week4/Task0.m
@@ -1,21 +1,24 @@
+%% ====================== Task 0 (Week 4) ====================== %%
+
 clear all
 close all
 clc
 
-dirname = 'train';
+dirname = 'train'; % Dirname of training images
 txtFiles = dir(fullfile([dirname '/gt'],'*.txt')); % Get all .txt files
-properties = getProperties(dirname, txtFiles);
+properties = getProperties(dirname, txtFiles); % Get the properties bounding box area and form factor
 
+% Get the maximum bounding box area for each shape
 area = struct('upTri', max(properties.A(:,1)), 'downTri', max(properties.B(:,1)), 'circle', max([max(properties.C(:,1)), max(properties.D(:,1)), max(properties.E(:,1))]),'square', max(properties.F(:,1)));
+% Get the mean form factor for each shape
 form_factor = struct('upTri', mean(properties.A(:,2)), 'downTri', mean(properties.B(:,2)), 'circle', mean([mean(properties.C(:,2)), mean(properties.D(:,2)), mean(properties.E(:,2))]),'square', mean(properties.F(:,2)));
 
-%%
+%% Create 4 templates (Binary, Color, Grayscale and Edge)
 
-% BW, Color, Gray
 [template_bw,template_color,template_gray,template_edge] = createTemplate(dirname, txtFiles, area, form_factor, properties);
 
-%%
-% Plot Up Triangle
+%% Plot Up Triangle
+
 figure;
 subplot(2,2,1)
 imshow(double(template_bw.upTri));
@@ -26,8 +29,8 @@ imshow(template_gray.upTri)
 subplot(2,2,4)
 imshow(template_edge.upTri)
 
-%%
-% Plot Down Triangle
+%% Plot Down Triangle
+
 figure;
 subplot(2,2,1)
 imshow(double(template_bw.downTri));
@@ -38,8 +41,8 @@ imshow(template_gray.downTri)
 subplot(2,2,4)
 imshow(template_edge.downTri)
 
-%%
-% Plot Circle
+%% Plot Circle
+
 figure;
 subplot(2,2,1)
 imshow(double(template_bw.circle));
@@ -50,8 +53,8 @@ imshow(template_gray.circle)
 subplot(2,2,4)
 imshow(template_edge.circle)
 
-%%
-% Plot Square
+%% Plot Square
+
 figure;
 subplot(2,2,1)
 imshow(double(template_bw.square));
