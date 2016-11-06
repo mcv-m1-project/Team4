8478ecf307be8b9bdfaa901cdedba816f5e506e5
tpereztorres <tonipereztorres@gmail.com>
2016-11-06 19:03:38 +0100

    Add files via upload

--- week4/createTemplate.m
+++ week4/createTemplate.m
@@ -1,37 +1,43 @@
 function [template_bw,template_color,template_gray,template_edge] = createTemplate(dirname, txtFiles, size, form_factor, prop)
     
-    % [wh wh] to resize image 
+    % get the dimension of each different shape depending on its size and
+    % form factor
     whU = round(sqrt(size.upTri .* form_factor.upTri));
     whD = round(sqrt(size.downTri .* form_factor.downTri));
     whC = round(sqrt(size.circle .* form_factor.circle));
     whS = round(sqrt(size.square .* form_factor.square));
+    % [wh wh] to resize image 
     
-    %croppedTemp = zeros([wh wh 3]);
-    %croppedTemp = 0;
-    gt = getGroundTruth(dirname, txtFiles);
+    gt = getGroundTruthT(dirname, txtFiles); % get the ground truth of all the .txt Files
     
-    up = 0; down = 0; circle = 0; sq = 0;
+    % Inicialization of all the variables that we will use inside the "while"
     tempU = 0; tempD = 0; tempC = 0; tempS = 0;
     tempiU = 0; tempiD = 0; tempiC = 0; tempiS = 0;
     
     for i = 1:length(txtFiles)
-        toSplit = strsplit(txtFiles(i).name,{'gt.','.txt'});
+        
+        toSplit = strsplit(txtFiles(i).name,{'gt.','.txt'}); % Split the String of the file's name
         fileTxt = fullfile([dirname '/gt'], strjoin(['gt.' toSplit(2) '.txt'],'')); % File by File
-        image = imread(strjoin([dirname, '/', toSplit(2), '.jpg'],''));
-        mask = imread(strjoin([dirname, '/mask/mask.', toSplit(2), '.png'],''));
+        image = imread(strjoin([dirname, '/', toSplit(2), '.jpg'],'')); % read the image
+        mask = imread(strjoin([dirname, '/mask/mask.', toSplit(2), '.png'],'')); % read the mask
         
-        [~, s] = LoadAnnotations(fileTxt);
+        [~, s] = LoadAnnotations(fileTxt); % We obtain the type of signal and the number of signals
         
-        j = 1;
+        j = 1; % Inicialization of j
         while j<=length(s)
+            
+            % Cut the part of the mask with the dimensions of the corresponent ground truth 
             croppedMask = double(imcrop(mask,[gt{i}(j).x, gt{i}(j).y, gt{i}(j).w, gt{i}(j).h]));
+            % The same with the image
             croppedIm = double(imcrop(image,[gt{i}(j).x, gt{i}(j).y, gt{i}(j).w, gt{i}(j).h]));
-            if s{j} == 'A' % upTri
-                m = imresize(croppedMask,[whU whU]);
-                im = imresize(croppedIm,[whU whU]);
-                tempU = tempU + m;
-                tempiU = tempiU + im;
-            elseif s{j} == 'B' % downTri
+            
+            if s{j} == 'A' % upTriangle (upTri)
+                % Change the size of the cropped mask to the dimensions width and height
+                m = imresize(croppedMask,[whU whU]); 
+                im = imresize(croppedIm,[whU whU]); % The same with the image
+                tempU = tempU + m; % Contains the sum of pixels (i,j) of each mask.
+                tempiU = tempiU + im; % The same with the image
+            elseif s{j} == 'B' % downTriangle (downTri)
                 m = imresize(croppedMask,[whD whD]);
                 im = imresize(croppedIm,[whD whD]);
                 tempD = tempD + m;
@@ -41,7 +47,7 @@ function [template_bw,template_color,template_gray,template_edge] = createTempla
                 im = imresize(croppedIm,[whS whS]);
                 tempS = tempS + m;
                 tempiS = tempiS + im;
-            elseif s{j} == 'C' || s{j} == 'D' || s{j} == 'E' % C, D, E (Circle)
+            elseif s{j} == 'C' || s{j} == 'D' || s{j} == 'E' % Circle
                 m = imresize(croppedMask,[whC whC]);
                 im = imresize(croppedIm,[whC whC]);
                 tempC = tempC + m;
@@ -51,16 +57,25 @@ function [template_bw,template_color,template_gray,template_edge] = createTempla
             j = j + 1;
             
         end
+        
         i
        
     end
     
+    % Calculate the template of the mask for each shape of signal (Up Triangle, Down Triangle, Circle and Square)
     template_bw = struct('upTri', round(uint8(tempU/length(prop.A))), 'downTri', round(uint8(tempD/length(prop.B))), 'circle', round(uint8(tempC/(length(prop.C) + length(prop.D) + length(prop.E)))), 'square', round(uint8(tempS/length(prop.F))));
-    save ('template_bw.mat', 'template_bw'); 
+    save ('template_bw.mat', 'template_bw'); % save the variable template as a .mat
+    
+    % Calculate the template of the image in color for each shape of signal (Up Triangle, Down Triangle, Circle and Square)
     template_color = struct('upTri', round(uint8(tempiU/length(prop.A))), 'downTri', round(uint8(tempiD/length(prop.B))), 'circle', round(uint8(tempiC/(length(prop.C) + length(prop.D) + length(prop.E)))), 'square', round(uint8(tempiS/length(prop.F))));
-    save ('template_color.mat', 'template_color'); 
+    save ('template_color.mat', 'template_color'); % save the variable template as a .mat
+    
+    % Calculate the template of the image in grayscale for each shape of signal (Up Triangle, Down Triangle, Circle and Square)
     template_gray = struct('upTri', rgb2gray(round(uint8(tempiU/length(prop.A)))), 'downTri', rgb2gray(round(uint8(tempiD/length(prop.B)))), 'circle', rgb2gray(round(uint8(tempiC/(length(prop.C) + length(prop.D) + length(prop.E))))), 'square', rgb2gray(round(uint8(tempiS/length(prop.F)))));
-    save ('template_gray.mat', 'template_gray'); 
+    save ('template_gray.mat', 'template_gray'); % save the variable template as a .mat
+    
+    % Calculate the template of the edges of the mask for each shape of signal (Up Triangle, Down Triangle, Circle and Square)
     template_edge = struct('upTri', edge(round(uint8(tempU/length(prop.A))),'Canny'), 'downTri', edge(round(uint8(tempD/length(prop.B))),'Canny'), 'circle', edge(round(uint8(tempC/(length(prop.C) + length(prop.D) + length(prop.E)))),'Canny'), 'square', edge(round(uint8(tempS/length(prop.F))),'Canny'));
-    save ('template_edge.mat', 'template_edge'); 
+    save ('template_edge.mat', 'template_edge'); % save the variable template as a .mat
+
 end
